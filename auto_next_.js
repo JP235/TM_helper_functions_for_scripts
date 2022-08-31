c4fb69292485fffc8129ed3c5357f52264b755f6
jpmanti <jpmanti@gmail.com>
2022-08-30 21:17:20 -0500

    remove function

--- auto_next_.js
+++ auto_next_.js
@@ -136,11 +136,11 @@ function delAll(na){
         };
 }
 
-function getDocHeight() {
-  var D = document;
-  return Math.max(
-      D.body.scrollHeight, D.documentElement.scrollHeight,
-      D.body.offsetHeight, D.documentElement.offsetHeight,
-      D.body.clientHeight, D.documentElement.clientHeight
-  );
-}
+// function getDocHeight() {
+//   var D = document;
+//   return Math.max(
+//       D.body.scrollHeight, D.documentElement.scrollHeight,
+//       D.body.offsetHeight, D.documentElement.offsetHeight,
+//       D.body.clientHeight, D.documentElement.clientHeight
+//   );
+// }
